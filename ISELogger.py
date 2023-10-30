d590213a16d13219b883d4f9a282a5cdbfbfe19e
CarisB <chkbaek@gmail.com>
2023-10-30 15:23:50 +0100

    Took off rounding for now because of numpy conflicts

--- ISELogger.py
+++ ISELogger.py
@@ -36,7 +36,7 @@ class ISELogger:
                 })
 
             # Return ISE info
-            return f"Voltage: {mv_value} mV, PPM: {round(ppm, 2)} ppm"
+            return f"Voltage: {mv_value} mV, PPM: {ppm} ppm"
 
         else:  # ISELogger.logging = False
             return LOGGER_DISABLED_MSG
