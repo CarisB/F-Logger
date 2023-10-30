1a3faeee91cd120dc6678434a234f760ca0a54de
CarisB <chkbaek@gmail.com>
2023-10-30 15:00:06 +0100

    Extrapolated all of GUI code into MainGUI class.

---
+++ MainGUI.py
@@ -0,0 +1,277 @@
+from config_data import *
+from config_gui import *
+from ISELogger import ISELogger
+from METEOLogger import METEOLogger
+from FMLogger import FMLogger
+from HVLogger import HVLogger
+
+import os
+import webbrowser
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import PhotoImage
+from tkinter import filedialog
+from tkinter import messagebox
+
+
+class MainGUI:
+    root: tk.Tk
+    hv_log_path = HV_LOG_DEFAULT_PATH  # Set the HV logfile path to the default
+
+    on_img: PhotoImage
+    off_img: PhotoImage
+
+    title_label: ttk.Label
+    toggle_all_frame: ttk.Frame
+    enable_all_button: ttk.Button
+    disable_all_button: ttk.Button
+
+    toggle_frame: ttk.Frame
+    ise_toggle_label: tk.Label
+    ise_toggle_button: ttk.Button
+    ise_status_text: tk.StringVar
+    ise_status_label: tk.Label
+    meteo_toggle_label: tk.Label
+    meteo_toggle_button: ttk.Button
+    meteo_status_text: tk.StringVar
+    meteo_status_label: tk.Label
+    fm_toggle_label: tk.Label
+    fm_toggle_button: ttk.Button
+    fm_status_text: tk.StringVar
+    fm_status_label: tk.Label
+    hv_toggle_label: tk.Label
+    hv_toggle_button: ttk.Button
+    hv_status_text: tk.StringVar
+    hv_status_label: tk.Label
+
+    hv_logfile_frame: ttk.Frame
+    hv_logfile_label: tk.Label
+    hv_logfile_text: tk.StringVar
+    hv_logfile_entry: tk.Entry
+    hv_logfile_browse_button: tk.Button
+    hv_logfile_select_button: tk.Button
+
+    db_write_status_text: tk.StringVar
+    db_write_status_label: tk.Label
+    grafana_button: ttk.Button
+
+    @classmethod
+    def init(cls):
+        cls.root = tk.Tk()
+        cls.root.title(WINDOW_TITLE)
+        cls.root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
+
+        # Resources
+        current_path = os.path.dirname(os.path.abspath(__file__))  # The current directory of the script
+        cls.on_img = PhotoImage(file=current_path + '/resources/on.png')
+        cls.off_img = PhotoImage(file=current_path + '/resources/off.png')
+
+        # Title
+        cls.title_label = ttk.Label(
+            master=cls.root, text=TITLE_LABEL, font=TITLE_FONT)
+        cls.title_label.pack(pady=20)
+
+        # Enable All / Disable All
+        cls.toggle_all_frame = ttk.Frame(master=cls.root)
+        cls.toggle_all_frame.pack(anchor='w', padx=30, pady=20)
+
+        cls.enable_all_button = ttk.Button(master=cls.toggle_all_frame, command=lambda: MainGUI.set_all(True),
+                                           text='Enable All')
+        cls.enable_all_button.pack(side='left')
+
+        cls.disable_all_button = ttk.Button(master=cls.toggle_all_frame, command=lambda: MainGUI.set_all(False),
+                                            text='Disable All')
+        cls.disable_all_button.pack(side='left', padx=20)
+
+        # Main Toggle Frame
+        cls.toggle_frame = ttk.Frame(master=cls.root)
+        cls.toggle_frame.pack(anchor='w', padx=30)
+        cls.toggle_frame.columnconfigure(index=0, weight=1)
+        cls.toggle_frame.columnconfigure(index=1, weight=1)
+        cls.toggle_frame.columnconfigure(index=2, weight=10)
+        cls.toggle_frame.rowconfigure(index=0, weight=1)
+        cls.toggle_frame.rowconfigure(index=1, weight=1)
+        cls.toggle_frame.rowconfigure(index=2, weight=1)
+        cls.toggle_frame.rowconfigure(index=3, weight=1)
+
+        # ISE Toggle Frame
+        cls.ise_toggle_label = tk.Label(master=cls.toggle_frame, text='ISE Logger',
+                                        anchor='w', font=TOGGLE_LABEL_FONT)
+        cls.ise_toggle_label.grid(column=0, row=0,
+                                  sticky=tk.W, padx=5, pady=5)
+
+        cls.ise_toggle_button = ttk.Button(master=cls.toggle_frame, image=cls.off_img, command=cls.toggle_ise_logger)
+        cls.ise_toggle_button.grid(column=1, row=0,
+                                   sticky=tk.W, padx=5, pady=5)
+
+        cls.ise_status_text = tk.StringVar()
+        cls.ise_status_label = tk.Label(master=cls.toggle_frame, textvariable=cls.ise_status_text)
+        cls.ise_status_label.grid(column=2, row=0,
+                                  sticky=tk.W, padx=50, pady=5)
+
+        # METEO Toggle Frame
+        cls.meteo_toggle_label = tk.Label(master=cls.toggle_frame, text='METEO Logger',
+                                          anchor='w', font=TOGGLE_LABEL_FONT)
+        cls.meteo_toggle_label.grid(column=0, row=1,
+                                    sticky=tk.W, padx=5, pady=5)
+
+        cls.meteo_toggle_button = ttk.Button(master=cls.toggle_frame, image=cls.off_img, command=cls.toggle_meteo_logger)
+        cls.meteo_toggle_button.grid(column=1, row=1,
+                                     sticky=tk.W, padx=5, pady=5)
+
+        cls.meteo_status_text = tk.StringVar()
+        cls.meteo_status_label = tk.Label(master=cls.toggle_frame, textvariable=cls.meteo_status_text)
+        cls.meteo_status_label.grid(column=2, row=1,
+                                    sticky=tk.W, padx=50, pady=5)
+
+        # FM Toggle Frame
+        cls.fm_toggle_label = tk.Label(master=cls.toggle_frame, text='FM Logger',
+                                       anchor='w', font=TOGGLE_LABEL_FONT)
+        cls.fm_toggle_label.grid(column=0, row=2,
+                                 sticky=tk.W, padx=5, pady=5)
+
+        cls.fm_toggle_button = ttk.Button(master=cls.toggle_frame, image=cls.off_img, command=cls.toggle_fm_logger)
+        cls.fm_toggle_button.grid(column=1, row=2,
+                                  sticky=tk.W, padx=5, pady=5)
+
+        cls.fm_status_text = tk.StringVar()
+        cls.fm_status_label = tk.Label(master=cls.toggle_frame, textvariable=cls.fm_status_text)
+        cls.fm_status_label.grid(column=2, row=2,
+                                 sticky=tk.W, padx=50, pady=5)
+
+        # HV Toggle Frame
+        cls.hv_toggle_label = tk.Label(master=cls.toggle_frame, text='HV Logger',
+                                       anchor='w', font=TOGGLE_LABEL_FONT)
+        cls.hv_toggle_label.grid(column=0, row=3,
+                                 sticky=tk.W, padx=5, pady=5)
+
+        cls.hv_toggle_button = ttk.Button(master=cls.toggle_frame, image=cls.off_img, command=cls.toggle_hv_logger)
+        cls.hv_toggle_button.grid(column=1, row=3,
+                                  sticky=tk.W, padx=5, pady=5)
+
+        cls.hv_status_text = tk.StringVar()
+        cls.hv_status_label = tk.Label(master=cls.toggle_frame, textvariable=cls.hv_status_text)
+        cls.hv_status_label.grid(column=2, row=3,
+                                 sticky=tk.W, padx=50, pady=5)
+
+        # HV Logfile
+        cls.hv_logfile_frame = ttk.Frame(master=cls.root)
+        cls.hv_logfile_frame.pack(pady=30)
+
+        cls.hv_logfile_label = tk.Label(master=cls.hv_logfile_frame, text='HV GECO Log File',
+                                        font='Helvetica 12 italic')
+        cls.hv_logfile_label.pack(anchor='w')
+
+        cls.hv_logfile_text = tk.StringVar()
+        cls.hv_logfile_text.set(cls.hv_log_path)
+        cls.hv_logfile_entry = tk.Entry(master=cls.hv_logfile_frame, textvariable=cls.hv_logfile_text,
+                                        width=50, fg='#333', bg='#eee')
+        cls.hv_logfile_entry.pack(side='left')
+
+        cls.hv_logfile_browse_button = tk.Button(master=cls.hv_logfile_frame, text='Browse Files...', command=cls.browse_files)
+        cls.hv_logfile_browse_button.pack(side='left')
+
+        cls.hv_logfile_select_button = tk.Button(master=cls.hv_logfile_frame, text='Select', command=cls.set_hv_log_path)
+        cls.hv_logfile_select_button.pack(side='left')
+
+        # DB Write Status
+        cls.db_write_status_text = tk.StringVar()
+        cls.db_write_status_label = tk.Label(master=cls.root, textvariable=cls.db_write_status_text,
+                                         padx=50, pady=10)
+        cls.db_write_status_label.pack(anchor='e')
+
+        # Link to Grafana
+        cls.grafana_button = ttk.Button(master=cls.root, command=cls.open_grafana,
+                                    text=GRAFANA_BUTTON_TEXT)
+        cls.grafana_button.pack(pady=10)
+
+    # region UI Functions
+    @classmethod
+    def toggle_ise_logger(cls):
+        if ISELogger.logging: cls.set_ise_logger(False)
+        else: cls.set_ise_logger(True)
+
+    @classmethod
+    def toggle_meteo_logger(cls):
+        if METEOLogger.logging: cls.set_meteo_logger(False)
+        else: cls.set_meteo_logger(True)
+
+    @classmethod
+    def toggle_fm_logger(cls):
+        if FMLogger.logging: cls.set_fm_logger(False)
+        else: cls.set_fm_logger(True)
+
+    @classmethod
+    def toggle_hv_logger(cls):
+        if HVLogger.logging: cls.set_hv_logger(False)
+        else: cls.set_hv_logger(True)
+
+    @classmethod
+    def set_all(cls, on: bool):
+        cls.set_ise_logger(on)
+        cls.set_meteo_logger(on)
+        cls.set_fm_logger(on)
+        cls.set_hv_logger(on)
+
+    @classmethod
+    def set_ise_logger(cls, on: bool):
+        if on:
+            cls.ise_toggle_button['image'] = cls.on_img
+            cls.ise_status_text.set(WAITING_TO_WRITE_MSG)
+            ISELogger.logging = True
+        else:
+            cls.ise_toggle_button['image'] = cls.off_img
+            cls.ise_status_text.set(LOGGER_DISABLED_MSG)
+            ISELogger.logging = False
+
+    @classmethod
+    def set_meteo_logger(cls, on: bool):
+        if on:
+            cls.meteo_toggle_button['image'] = cls.on_img
+            cls.meteo_status_text.set(WAITING_TO_WRITE_MSG)
+            METEOLogger.logging = True
+        else:
+            cls.meteo_toggle_button['image'] = cls.off_img
+            cls.meteo_status_text.set(LOGGER_DISABLED_MSG)
+            METEOLogger.logging = False
+
+    @classmethod
+    def set_fm_logger(cls, on: bool):
+        if on:
+            cls.fm_toggle_button['image'] = cls.on_img
+            cls.fm_status_text.set(WAITING_TO_WRITE_MSG)
+            FMLogger.logging = True
+        else:
+            cls.fm_toggle_button['image'] = cls.off_img
+            cls.fm_status_text.set(LOGGER_DISABLED_MSG)
+            FMLogger.logging = False
+
+    @classmethod
+    def set_hv_logger(cls, on: bool):
+        if on:
+            cls.hv_toggle_button['image'] = cls.on_img
+            cls.hv_status_text.set(WAITING_TO_WRITE_MSG)
+            HVLogger.logging = True
+        else:
+            cls.hv_toggle_button['image'] = cls.off_img
+            cls.hv_status_text.set(LOGGER_DISABLED_MSG)
+            HVLogger.logging = False
+
+    @classmethod
+    def browse_files(cls):
+        filename = filedialog.askopenfilename(
+            initialdir='/',
+            title='Select a File',
+            filetypes=(('.log files', '*.log'), ('All files', '*.*'))
+        )
+        cls.hv_logfile_text.set(filename)
+
+    @classmethod
+    def set_hv_log_path(cls):
+        MainGUI.hv_log_path = cls.hv_logfile_text.get()
+        tk.messagebox.showinfo(title='Success', message=f'HV logfile set to {cls.hv_log_path}')
+
+    @staticmethod
+    def open_grafana():
+        webbrowser.open_new(GRAFANA_URL)
--- main.py
+++ main.py
@@ -6,10 +6,9 @@
 Sensors are stored as static variables in the Sensors class.
 Each logger has its own static class that stores its respective logging state (HVLogger also has HVLogger.line).
 The logger's add_data function adds the log data to data_points[] and returns a status string.
-main() is run initially by tkinter.after(0, main), and then run recursively by itself with tkinter.after(POLLING_MS, main)
+main() is first run by root.after(0, main), and then run recursively by itself with root.after(POLLING_MS, main)
 """
 
-# region Imports
 from config_db import *
 from config_data import *
 from config_gui import *
@@ -18,260 +17,23 @@ from ISELogger import ISELogger
 from METEOLogger import METEOLogger
 from FMLogger import FMLogger
 from HVLogger import HVLogger
+from MainGUI import MainGUI
 
 from influxdb import InfluxDBClient
-import os
-import webbrowser
+
 from datetime import datetime
-
-import tkinter as tk
-from tkinter import ttk
-from tkinter import PhotoImage
-from tkinter import filedialog
-from tkinter import messagebox
-
 import warnings
 from urllib3.exceptions import InsecureRequestWarning
 warnings.filterwarnings("ignore", category=InsecureRequestWarning)
-# endregion
 
 
-# region Init
 # Open connection with database
 client = InfluxDBClient(host=DB_HOST, port=DB_PORT, username=DB_USERNAME, password=DB_PASSWORD,
                         database=DB_DATABASE, ssl=True, verify_ssl=False)
-
-hv_log_path = HV_LOG_DEFAULT_PATH  # Set the HV logfile path to the default
-
 Sensors.init()  # Find and initialize sensors
-# endregion
+MainGUI.init()  # Initialize GUI
 
 
-# region UI Functions
-def toggle_ise_logger():
-    if ISELogger.logging: set_ise_logger(False)
-    else: set_ise_logger(True)
-
-
-def toggle_meteo_logger():
-    if METEOLogger.logging: set_meteo_logger(False)
-    else: set_meteo_logger(True)
-
-
-def toggle_fm_logger():
-    if FMLogger.logging: set_fm_logger(False)
-    else: set_fm_logger(True)
-
-
-def toggle_hv_logger():
-    if HVLogger.logging: set_hv_logger(False)
-    else: set_hv_logger(True)
-
-
-def set_all(on: bool):
-    set_ise_logger(on)
-    set_meteo_logger(on)
-    set_fm_logger(on)
-    set_hv_logger(on)
-
-
-def set_ise_logger(on: bool):
-    if on:
-        ise_toggle_button['image'] = on_img
-        ise_status_text.set(WAITING_TO_WRITE_MSG)
-        ISELogger.logging = True
-    else:
-        ise_toggle_button['image'] = off_img
-        ise_status_text.set(LOGGER_DISABLED_MSG)
-        ISELogger.logging = False
-
-
-def set_meteo_logger(on: bool):
-    if on:
-        meteo_toggle_button['image'] = on_img
-        meteo_status_text.set(WAITING_TO_WRITE_MSG)
-        METEOLogger.logging = True
-    else:
-        meteo_toggle_button['image'] = off_img
-        meteo_status_text.set(LOGGER_DISABLED_MSG)
-        METEOLogger.logging = False
-
-
-def set_fm_logger(on: bool):
-    if on:
-        fm_toggle_button['image'] = on_img
-        fm_status_text.set(WAITING_TO_WRITE_MSG)
-        FMLogger.logging = True
-    else:
-        fm_toggle_button['image'] = off_img
-        fm_status_text.set(LOGGER_DISABLED_MSG)
-        FMLogger.logging = False
-
-
-def set_hv_logger(on: bool):
-    if on:
-        hv_toggle_button['image'] = on_img
-        hv_status_text.set(WAITING_TO_WRITE_MSG)
-        HVLogger.logging = True
-    else:
-        hv_toggle_button['image'] = off_img
-        hv_status_text.set(LOGGER_DISABLED_MSG)
-        HVLogger.logging = False
-
-
-def browse_files():
-    global hv_log_path
-
-    filename = filedialog.askopenfilename(
-        initialdir='/',
-        title='Select a File',
-        filetypes=(('.log files', '*.log'), ('All files', '*.*'))
-    )
-    hv_logfile_text.set(filename)
-
-
-def set_hv_log_path():
-    global hv_log_path
-
-    hv_log_path = hv_logfile_text.get()
-    tk.messagebox.showinfo(title='Success', message=f'HV logfile set to {hv_log_path}')
-
-
-def open_grafana():
-    webbrowser.open_new(GRAFANA_URL)
-# endregion
-
-
-# region GUI
-# Main window
-root = tk.Tk()
-root.title(WINDOW_TITLE)
-root.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}')
-
-# Resources
-current_path = os.path.dirname(os.path.abspath(__file__))  # The current directory of the script
-on_img = PhotoImage(file=current_path + '/resources/on.png')
-off_img = PhotoImage(file=current_path + '/resources/off.png')
-
-# Title
-title_label = ttk.Label(
-    master=root, text=TITLE_LABEL, font=TITLE_FONT)
-title_label.pack(pady=20)
-
-# Enable All / Disable All
-toggle_all_frame = ttk.Frame(master=root)
-toggle_all_frame.pack(anchor='w', padx=30, pady=20)
-
-enable_all_button = ttk.Button(master=toggle_all_frame, text="Enable All", command=lambda:set_all(True))
-enable_all_button.pack(side='left')
-
-disable_all_button = ttk.Button(master=toggle_all_frame, text="Disable All", command=lambda:set_all(False))
-disable_all_button.pack(side='left', padx=20)
-
-# Main Toggle Frame
-toggle_frame = ttk.Frame(master=root)
-toggle_frame.pack(anchor='w', padx=30)
-toggle_frame.columnconfigure(index=0, weight=1)
-toggle_frame.columnconfigure(index=1, weight=1)
-toggle_frame.columnconfigure(index=2, weight=10)
-toggle_frame.rowconfigure(index=0, weight=1)
-toggle_frame.rowconfigure(index=1, weight=1)
-toggle_frame.rowconfigure(index=2, weight=1)
-toggle_frame.rowconfigure(index=3, weight=1)
-
-# ISE Toggle Frame
-ise_toggle_label = tk.Label(master=toggle_frame, text='ISE Logger',
-                            anchor='w', font=TOGGLE_LABEL_FONT)
-ise_toggle_label.grid(column=0, row=0,
-                      sticky=tk.W, padx=5, pady=5)
-
-ise_toggle_button = ttk.Button(master=toggle_frame, image=off_img, command=toggle_ise_logger)
-ise_toggle_button.grid(column=1, row=0,
-                       sticky=tk.W, padx=5, pady=5)
-
-ise_status_text = tk.StringVar()
-ise_status_label = ttk.Label(master=toggle_frame, textvariable=ise_status_text)
-ise_status_label.grid(column=2, row=0,
-                      sticky=tk.W, padx=50, pady=5)
-
-# METEO Toggle Frame
-meteo_toggle_label = tk.Label(master=toggle_frame, text='METEO Logger',
-                              anchor='w', font=TOGGLE_LABEL_FONT)
-meteo_toggle_label.grid(column=0, row=1,
-                        sticky=tk.W, padx=5, pady=5)
-
-meteo_toggle_button = ttk.Button(master=toggle_frame, image=off_img, command=toggle_meteo_logger)
-meteo_toggle_button.grid(column=1, row=1,
-                         sticky=tk.W, padx=5, pady=5)
-
-meteo_status_text = tk.StringVar()
-meteo_status_label = ttk.Label(master=toggle_frame, textvariable=meteo_status_text)
-meteo_status_label.grid(column=2, row=1,
-                        sticky=tk.W, padx=50, pady=5)
-
-# FM Toggle Frame
-fm_toggle_label = tk.Label(master=toggle_frame, text='FM Logger',
-                           anchor='w', font=TOGGLE_LABEL_FONT)
-fm_toggle_label.grid(column=0, row=2,
-                     sticky=tk.W, padx=5, pady=5)
-
-fm_toggle_button = ttk.Button(master=toggle_frame, image=off_img, command=toggle_fm_logger)
-fm_toggle_button.grid(column=1, row=2,
-                      sticky=tk.W, padx=5, pady=5)
-
-fm_status_text = tk.StringVar()
-fm_status_label = ttk.Label(master=toggle_frame, textvariable=fm_status_text)
-fm_status_label.grid(column=2, row=2,
-                     sticky=tk.W, padx=50, pady=5)
-
-# HV Toggle Frame
-hv_toggle_label = tk.Label(master=toggle_frame, text='HV Logger',
-                           anchor='w', font=TOGGLE_LABEL_FONT)
-hv_toggle_label.grid(column=0, row=3,
-                     sticky=tk.W, padx=5, pady=5)
-
-hv_toggle_button = ttk.Button(master=toggle_frame, image=off_img, command=toggle_hv_logger)
-hv_toggle_button.grid(column=1, row=3,
-                      sticky=tk.W, padx=5, pady=5)
-
-hv_status_text = tk.StringVar()
-hv_status_label = ttk.Label(master=toggle_frame, textvariable=hv_status_text)
-hv_status_label.grid(column=2, row=3,
-                     sticky=tk.W, padx=50, pady=5)
-
-# HV Logfile
-hv_logfile_frame = ttk.Frame(master=root)
-hv_logfile_frame.pack(pady=30)
-
-hv_logfile_label = tk.Label(master=hv_logfile_frame, text='HV GECO Log File',
-                            font='Helvetica 12 italic')
-hv_logfile_label.pack(anchor='w')
-
-hv_logfile_text = tk.StringVar()
-hv_logfile_text.set(hv_log_path)
-hv_logfile_entry = tk.Entry(master=hv_logfile_frame, textvariable=hv_logfile_text,
-                            width=50, fg='#333', bg='#eee')
-hv_logfile_entry.pack(side='left')
-
-hv_logfile_browse_button = tk.Button(master=hv_logfile_frame, text='Browse Files...', command=browse_files)
-hv_logfile_browse_button.pack(side='left')
-
-hv_logfile_select_button = tk.Button(master=hv_logfile_frame, text='Select', command=set_hv_log_path)
-hv_logfile_select_button.pack(side='left')
-
-# DB Write Status
-db_write_status_text = tk.StringVar()
-db_write_status_label = tk.Label(master=root, textvariable=db_write_status_text,
-                                 padx=50, pady=10)
-db_write_status_label.pack(anchor='e')
-
-# Link to Grafana
-grafana_button = ttk.Button(master=root, text=GRAFANA_BUTTON_TEXT, command=open_grafana)
-grafana_button.pack(pady=10)
-# endregion
-
-
-# region Main Logic
 # Main loop (called back by tkinter.after() after POLLING_MS, set in the config_data module)
 def main():
     data_points = []  # Resets data collection to write to database
@@ -279,17 +41,17 @@ def main():
 
     # Gets the last line of the selected HV logfile
     if HVLogger.logging:
-        HVLogger.line = HVLogger.get_last_line(hv_log_path)
+        HVLogger.line = HVLogger.get_last_line(MainGUI.hv_log_path)
 
     # Add data points and set status text accordingly
-    ise_status_text.set(
+    MainGUI.ise_status_text.set(
         ISELogger.add_data(data_points))
-    meteo_status_text.set(
+    MainGUI.meteo_status_text.set(
         METEOLogger.add_data(data_points))
-    hv_status_text.set(
-        HVLogger.add_data(data_points))
-    fm_status_text.set(
+    MainGUI.fm_status_text.set(
         FMLogger.add_data(data_points))
+    MainGUI.hv_status_text.set(
+        HVLogger.add_data(data_points))
 
     # Timestamp
     current_time = datetime.now()
@@ -297,17 +59,16 @@ def main():
     timestamp_str = timestamp.strftime("[%H:%M:%S]: ")
 
     if data_points and client.write_points(data_points):  # Successful write
-        db_write_status_text.set(timestamp_str + WRITE_SUCCESS_TEXT)
-        db_write_status_label['fg'] = WRITE_SUCCESS_COLOR
+        MainGUI.db_write_status_text.set(timestamp_str + WRITE_SUCCESS_TEXT)
+        MainGUI.db_write_status_label['fg'] = WRITE_SUCCESS_COLOR
     else:  # Couldn't write to database
-        db_write_status_text.set(timestamp_str + WRITE_FAIL_TEXT)
-        db_write_status_label['fg'] = WRITE_FAIL_COLOR
+        MainGUI.db_write_status_text.set(timestamp_str + WRITE_FAIL_TEXT)
+        MainGUI.db_write_status_label['fg'] = WRITE_FAIL_COLOR
 
     # Waits for POLLING_MS milliseconds, then callback to repeat loop
-    root.after(POLLING_MS, main)
+    MainGUI.root.after(POLLING_MS, main)
 
 
 # Run
-root.after(0, main)
-root.mainloop()
-# endregion
+MainGUI.root.after(0, main)
+MainGUI.root.mainloop()
